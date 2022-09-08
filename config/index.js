c1433552a209fb6084ef3b8aed1cb829c503d163
Star <113099388+zhengwei196615@users.noreply.github.com>
2022-09-08 19:28:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,22 +18,14 @@ export const config = {
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "放牛小贝",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "oloJU6CWWL1ddjgp0tq3IOAlRuFA",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "lyMWSrgZ8YPqeFGJYaB7zFbuvOarXTCouOkWZkV_CoY",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已
-   *   }, 
-   {
-   *    // 想要发送的人的名字
    *    name: "养猪大户",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "oloJU6Cm5-4CqhYey4aaPz1gjrUU",
+   *    id: "gh_57501f9fc142 ",
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "lyMWSrgZ8YPqeFGJYaB7zFbuvOarXTCouOkWZkV_CoY",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已
    *   }, 
+  
    * ]
    */
   USERS: [
@@ -52,19 +44,7 @@ export const config = {
       openUrl: "https://s.weibo.com/top/summary/"
     }, 
     
-    // 想要发送的人的名字
-      name: "养猪大户",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oloJU6Cm5-4CqhYey4aaPz1gjrUU",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "lyMWSrgZ8YPqeFGJYaB7zFbuvOarXTCouOkWZkV_CoY",
-      // 所在省份
-      province: "陕西",
-      // 所在城市
-      city: "杨凌",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://s.weibo.com/top/summary/"
-    }, 
+   
     
   ],
 
