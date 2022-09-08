114418993f99fb112c2e7d4c15a101eeaa586140
Star <113099388+zhengwei196615@users.noreply.github.com>
2022-09-08 19:16:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,30 +43,7 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://s.weibo.com/top/summary/"
     }, 
-    {
-      name: "养猪大户", 
-      id: "oWbBN5lbLJbD6E93xnUJWhZ98rjU",
-      useTemplateId: "PnO-uiJH5uTzTqYBf-oPMhZCXnTRDwOzGuuMT2Y_MrM",
-      province: "陕西",
-      city: "杨凌",
-      openUrl: "https://s.weibo.com/top/summary/"
-    }, 
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
+    
   ],
 
   /**
